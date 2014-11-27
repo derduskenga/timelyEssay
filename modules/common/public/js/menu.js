ffb870ffd7bf8b8d1a15b3856e6b70b68a696745
Sam Wambua <wambua.sam@gmail.com>
2014-11-27 15:10:43 +0300

    Added active class to selected home menu.

--- modules/common/public/js/menu.js
+++ modules/common/public/js/menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	makeLinkActive();
+	makeHomeLinkMenuActive();
 });
 function makeLinkActive(){
 	var path = location.pathname;
@@ -7,5 +8,13 @@ function makeLinkActive(){
 				if($(this).attr('href')==path){
 						$(this).addClass("active");
 				}
+	});
+}
+
+function makeHomeLinkMenuActive(){
+	var path = location.pathname;
+	$('#navigation > li > a').each(function(e){
+				if($(this).attr('href')==path)
+					$(this).parent().addClass("active");
 	});
 }
